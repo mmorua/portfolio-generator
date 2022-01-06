5c404d498bb01469e32c8859939a951bd162cc09
Marcus Morua <marcus.morua@gmail.com>
2022-01-06 00:04:21 -0600

    feat: Generate HTML file via Node.js

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-// Notice the lack of parentheses around the profileDataArr parameter
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-};
-console.log('================');
-// Is the same as this... Notice the lack of parentheses around the profileItem parameter
-profileDataArr.forEach(profileItem => console.log(profileItem));
+const generatePage = require('./src/page-template.js');
 
+const profileDataArgs = process.argv.slice(2);
 
+const [handle, github] = profileDataArgs;
 
-
-
-
-
-printProfileData(profileDataArgs)
+fs.writeFile('./index.html', generatePage(handle, github), err => {
+      if (err) throw err;
+      console.log('Portfolio complete! Check out index.html to see the output!');
+  });
